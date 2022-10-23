99e06d86556b9b8d6487472daeb1044a953bbc2e
dimm <witty.sock3113@fastmail.com>
2022-10-23 15:07:23 +0200

    analise string as array

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	s := "abcccccdd"
+	fmt.Println(longestPalindrome(s))
 }
 
 func longestPalindrome(s string) int {
+	uneven := 0
+	lenth := len(s)
 
+	if lenth < 2 {
+		return lenth
+	}
+
+	if lenth%2 != 0 {
+		uneven = 1
+	}
+
+	var table [60]int
+
+	for _, v := range s {
+		table[v-65] += 1
+	}
+	return 3 + uneven
 }
